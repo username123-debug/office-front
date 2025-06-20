70c4679e547d1c32f440cc902c62f997506b525c
mahiro.furuta <mahiro.furuta@gigxit.co.jp>
2025-06-20 15:09:01 +0900

    SecurityConfigに追加

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -36,11 +36,12 @@ public class SecurityConfig {
                 .requestMatchers("/api/login/**").permitAll() //もしもログイン後のURLであれば、.requestMatchers().authenticated() 
                 .requestMatchers(  "/users/**" ) .authenticated()       // ユーザー系（/me, /{id}, /users など）
                 .requestMatchers( "/schedules/**") .authenticated() 	// スケジュール系
+                .requestMatchers( "/Info/**") .authenticated()       // お知らせ一覧・登録・削除など
                 .requestMatchers( "/attendance/**" ) .authenticated()   // 勤怠打刻API（出勤・退勤）（未実装）
                 .requestMatchers( "/notices/**") .authenticated()       // お知らせ一覧・登録・削除など
                 .anyRequest()
                 .authenticated()
-            )
+            
             .formLogin(form -> form
                 .loginProcessingUrl("/api/login")
                 .successHandler(customLoginSuccessHandler) //ログイン成功時の処理 
