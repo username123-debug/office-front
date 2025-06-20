a75bdebd166c4d9573f0a5ca3230f26420733033
mahiro.furuta <mahiro.furuta@gigxit.co.jp>
2025-06-20 11:27:06 +0900

    ScheduleControllerに更新、削除のAPIを追加

--- src/main/java/com/example/demo/controller/ScheduleController.java
+++ src/main/java/com/example/demo/controller/ScheduleController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,5 +63,44 @@ public class ScheduleController {
 //        }
 //
 //        return ResponseEntity.ok(schedules);
+    
+    
+    //更新
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateSchedule(@PathVariable Long id, @RequestBody ScheduleRequestDto dto) {
+        Optional<Schedule> optionalSchedule = scheduleRepository.findById(id);
+
+        if (optionalSchedule.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Schedule schedule = optionalSchedule.get();
+
+        // 参加者をIDから取得
+        List<User> participants = userRepository.findAllById(dto.getParticipantIds());
+
+        // 更新内容をセット
+        schedule.setTitle(dto.getTitle());
+        schedule.setDescription(dto.getDescription());
+        schedule.setStartDateTime(dto.getStartDateTime());
+        schedule.setEndDateTime(dto.getEndDateTime());
+        schedule.setParticipants(participants);
+
+        return ResponseEntity.ok(scheduleRepository.save(schedule));
+        
+    }
+    
+    //削除
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteSchedule(@PathVariable Long id) {
+        if (!scheduleRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        scheduleRepository.deleteById(id);
+        return ResponseEntity.ok("削除しました");
+    }
+
+
 }
 
